f440fc0f8d06b81a390b6630a4b963ffe7adcfa9
Stefan Sundkvist <dev@sundkvist.eu>
2017-06-09 21:21:11 +0200

    skip commits

--- vars/push_to_azure.groovy
+++ vars/push_to_azure.groovy
@@ -1,5 +1,7 @@
 def call(String name) {
     bat """
-          git push -f "https://%GIT_USERNAME%:%GIT_PASSWORD%@${name}.scm.azurewebsites.net:443/${name}.git" master
+          git checkout azure
+          git branch azure
+          git push -f "https://%GIT_USERNAME%:%GIT_PASSWORD%@${name}.scm.azurewebsites.net:443/${name}.git" azure
         """
 }

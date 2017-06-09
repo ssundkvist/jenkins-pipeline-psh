04e664346f097da0984650e07591926af610daf7
Stefan Sundkvist <dev@sundkvist.eu>
2017-06-09 18:26:53 +0200

    try double quotes

--- vars/push_to_azure.groovy
+++ vars/push_to_azure.groovy
@@ -1,9 +1,9 @@
 def call(String name) {
-    bat '''
+    bat """
           git checkout azure
           git branch azure
           git add .funcpack
           git commit -m 'Updated'
-          git push -f "https://%GIT_USERNAME%:%GIT_PASSWORD%@%name%.scm.azurewebsites.net:443/%name%.git" azure
-        '''
+          git push -f "https://%GIT_USERNAME%:%GIT_PASSWORD%@${name}.scm.azurewebsites.net:443/${name}.git" azure
+        """
 }

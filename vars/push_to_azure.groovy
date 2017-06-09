3f4360b61cae49b5ba21b0e051daa56a4e8beb2d
Stefan Sundkvist <dev@sundkvist.eu>
2017-06-09 19:30:13 +0200

    debug git username

--- vars/push_to_azure.groovy
+++ vars/push_to_azure.groovy
@@ -4,6 +4,7 @@ def call(String name) {
           git branch azure
           git add .funcpack
           git commit -m 'Updated'
+          echo %GIT_USERNAME%
           git push -f "https://%GIT_USERNAME%:%GIT_PASSWORD%@${name}.scm.azurewebsites.net:443/${name}.git" azure
         """
 }

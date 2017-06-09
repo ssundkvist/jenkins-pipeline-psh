6cebf2f7e65cf121e627681a835f4ab444b3a042
Stefan Sundkvist <dev@sundkvist.eu>
2017-06-09 22:20:02 +0200

    troubleshoot

--- vars/push_to_azure.groovy
+++ vars/push_to_azure.groovy
@@ -1,7 +1,7 @@
 def call(String username, String password, String name) {
     bat """
-        FOR /f %%i IN ('git branch --list azure') DO SET BRANCH_EXISTS=%%i
-        IF DEFINED BRANCH_EXISTS (git checkout azure) ELSE (git checkout -b azure)
+        REM FOR /f %%i IN ('git branch --list azure') DO SET BRANCH_EXISTS=%%i
+        REM IF DEFINED BRANCH_EXISTS (git checkout azure) ELSE (git checkout -b azure)
         git push -f "https://${username}:${password}@${name}.scm.azurewebsites.net:443/${name}.git" azure
     """
 }

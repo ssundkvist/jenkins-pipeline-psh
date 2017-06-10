4dfcfeb836cee2765240baac510d3788b8218143
Stefan Sundkvist <sst@kth.se>
2017-06-10 10:44:30 +0200

    create or reset azure branch

--- vars/push_to_azure.groovy
+++ vars/push_to_azure.groovy
@@ -1,7 +1,6 @@
 def call(String name) {
     bat """
-        FOR /f %%i IN ('git branch --list azure') DO SET BRANCH_EXISTS=%%i
-        IF DEFINED BRANCH_EXISTS (git checkout azure) ELSE (git checkout -b azure)
+        git checkout -B azure)
         git push -f "https://%GIT_USERNAME%:%GIT_PASSWORD%@${name}.scm.azurewebsites.net:443/${name}.git" azure:master
     """
 }

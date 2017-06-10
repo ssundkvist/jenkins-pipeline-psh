10fa9ba0629ed9631f81bd6f3ef83367b499308a
Stefan Sundkvist <dev@sundkvist.eu>
2017-06-10 10:08:42 +0200

    push to master

--- vars/push_to_azure.groovy
+++ vars/push_to_azure.groovy
@@ -2,6 +2,6 @@ def call(String name) {
     bat """
         FOR /f %%i IN ('git branch --list azure') DO SET BRANCH_EXISTS=%%i
         IF DEFINED BRANCH_EXISTS (git checkout azure) ELSE (git checkout -b azure)
-        git push -f "https://%GIT_USERNAME%:%GIT_PASSWORD%@${name}.scm.azurewebsites.net:443/${name}.git" azure
+        git push -f "https://%GIT_USERNAME%:%GIT_PASSWORD%@${name}.scm.azurewebsites.net:443/${name}.git" azure:master
     """
 }

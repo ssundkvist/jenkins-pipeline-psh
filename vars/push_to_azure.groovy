1f0aba96f83c137e3960bac7e23bbf30391b22b1
Stefan Sundkvist <dev@sundkvist.eu>
2017-09-18 18:07:34 +0200

    Update for deployment slots

--- vars/push_to_azure.groovy
+++ vars/push_to_azure.groovy
@@ -2,6 +2,6 @@ def call(String name) {
     bat """
         git branch -d azure)
         git checkout -B azure
-        git push -f "https://%GIT_USERNAME%:%GIT_PASSWORD%@${name}.scm.azurewebsites.net:443/${name}.git" azure:master
+        git push -f "https://%GIT_USERNAME%:%GIT_PASSWORD%@${name}-stage.scm.azurewebsites.net:443/${name}.git" azure:master
     """
 }
